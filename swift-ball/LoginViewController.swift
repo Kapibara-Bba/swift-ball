f88f1647f87c021cefd2515e242b6ac99e7d93a2
Kapibara-Bba <kapibara.bba0924@gmail.com>
2024-08-06 08:23:33 +0900

    NewViewController作成

---
+++ swift-ball/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  swift-ball
+//
+//  Created by Masaya Furukawa on 2024/08/06.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
