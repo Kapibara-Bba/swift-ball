92c34715090e0eba6e03ff024f81f13194307ad3
Kapibara-Bba <kapibara.bba0924@gmail.com>
2024-08-07 23:36:16 +0900

    トップ画面

---
+++ MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  swift-ball
+//
+//  Created by Masaya Furukawa on 2024/08/07.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
