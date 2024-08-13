ac76f4eb650be4c580e7b0e96f2d7d0b9099aa94
Kapibara-Bba <kapibara.bba0924@gmail.com>
2024-08-13 23:20:27 +0900

    スコア登録画面

---
+++ ScoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScoreViewController.swift
+//  swift-ball
+//
+//  Created by Masaya Furukawa on 2024/08/13.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
