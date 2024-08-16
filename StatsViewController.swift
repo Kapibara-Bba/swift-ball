083e2141486a731677ff225a5bacb54c3f52f77b
Kapibara-Bba <kapibara.bba0924@gmail.com>
2024-08-16 19:26:10 +0900

    個人成績登録画面

---
+++ StatsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StatsViewController.swift
+//  swift-ball
+//
+//  Created by Masaya Furukawa on 2024/08/16.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
